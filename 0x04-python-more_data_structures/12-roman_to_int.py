72abbf73e14a02a3a4ec6966f2b5361cfba29473
patty6339 <patty6339@gmail.com>
2023-01-11 12:58:33 -0800

    updated 2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,34 @@
-""" 
-Roman to Integer test file
+#!/usr/bin/python3
+def roman_value(prmCharacter):
+    roman_list = [('I', 1), ('V', 5), ('X', 10),
+                  ('L', 50), ('C', 100), ('D', 500), ('M', 1000)]
+    for item in roman_list:
+        character, value = item
+        if (prmCharacter is character):
+            return value
+    return None
 
-"""
-roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+def next_value(prmString, prmIndex):
+    if prmIndex + 1 < len(prmString):
+        return roman_value(prmString[prmIndex + 1])
+    else:
+        return None
 
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+def roman_to_int(roman_string):
+    result = 0
 
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+    if (roman_string is None or isinstance(roman_string, str) is False):
+        return result
 
+    enum = enumerate(roman_string)
+    for index, character in enum:
+        currentValue = roman_value(character)
+        nextValue = next_value(roman_string, index)
+        if nextValue is None or currentValue >= nextValue:
+            result += currentValue
+        else:
+            result += (nextValue - currentValue)
+            next(enum)
+    return result
