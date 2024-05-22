34128b0d1f3393ee05ea9a01f87ba7ed7fa1ba39
Patrick <patty6339@gmail.com>
2024-05-22 10:42:55 -0700

    amended

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,10 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+-- Check if the user user_0d_1 already exists
+SELECT COUNT(*) INTO @user_exists FROM mysql.user WHERE User = 'user_0d_1';
+
+-- If the user doesn't exist, create it and grant all privileges
+IF @user_exists = 0 THEN
+    CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+    FLUSH PRIVILEGES;
+END IF;
 
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,17 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+-- Check if the database hbtn_0d_2 exists
+SELECT EXISTS(SELECT datname FROM pg_database WHERE datname = 'hbtn_0d_2') INTO @db_exists;
+
+-- If the database doesn't exist, create it
+IF NOT @db_exists THEN
+    CREATE DATABASE hbtn_0d_2;
+END IF;
+
+-- Check if the user user_0d_2 exists
+SELECT EXISTS(SELECT rolname FROM pg_roles WHERE rolname = 'user_0d_2') INTO @user_exists;
+
+-- If the user doesn't exist, create it and grant SELECT privilege on hbtn_0d_2
+IF NOT @user_exists THEN
+    CREATE USER user_0d_2 WITH PASSWORD 'user_0d_2_pwd';
+    GRANT SELECT ON DATABASE hbtn_0d_2 TO user_0d_2;
+END IF;
+
