b853675273e6afaec273e721919c1fe6aeceeb89
Patrick <patty6339@gmail.com>
2024-07-08 19:37:02 +0300

    py 0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# script that makes a request to 0.0.0.0:5000/catch_me
+#!/bin/bash # script to make a request to 0.0.0.0:5000/catch_me
 curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
+
